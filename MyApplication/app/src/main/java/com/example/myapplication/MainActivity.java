707b91d4902841226468f7302706cf9bc5905863
remasmarafie2022 <110308680+remasmarafie2022@users.noreply.github.com>
2022-08-05 17:08:01 +0300

    done

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button remas = findViewById(R.id.button2);
+
+    }
+}
